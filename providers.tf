cdcf7c63a3bd189be96a36c7da83de5551769603
Srujan153 <103749117+Srujan153@users.noreply.github.com>
2022-04-15 19:37:54 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 provider "aws" {
  region = "us-east-1"
- access_key =  "AKIA6EUAV2DF7SM73H5X"
- secret_key = "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"  
+ access_key =  "AKIA6EUAV2DFY6ZNZ6WP"
+ secret_key = "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"  
   
 }
